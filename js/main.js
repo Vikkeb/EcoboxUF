09d42e7f4c2e18df9a8fd819190e071f43c21e2c
Victor Bergsten <victor.bergsten@ericsson.com>
2017-09-13 15:57:11 +0200

    css, js, html updates

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 ;(function () {
-	
+
 	'use strict';
 
-
-
-	// iPad and iPod detection	
+	// iPad and iPod detection
 	var isiPad = function(){
 		return (navigator.platform.indexOf("iPad") != -1);
 	};
 
 	var isiPhone = function(){
 	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPhone") != -1) ||
 			(navigator.platform.indexOf("iPod") != -1)
 	    );
 	};
@@ -91,7 +89,7 @@
 		$('.animate-box').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
+
 				i++;
 
 				$(this.element).addClass('item-animate');
@@ -110,9 +108,9 @@
 							el.removeClass('item-animate');
 						},  k * 200, 'easeInOutExpo' );
 					});
-					
+
 				}, 100);
-				
+
 			}
 
 		} , { offset: '85%' } );
@@ -121,12 +119,12 @@
 	var parallax = function() {
 		$(window).stellar({
 			horizontalScrolling: false,
-			hideDistantElements: false, 
+			hideDistantElements: false,
 			responsive: true
 
 		});
 	};
-	
+
 
 
 	// Document on load.
